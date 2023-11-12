b7bbc3b26e22ef7e230afa9397c7e75604109890
Daniel <danicraft.op7@gamil.com>
2023-11-12 14:58:30 -0500

    Este es el primer commit del proyecto

---
+++ src/main/java/MiniProyecto3/CuentasUsuarios.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MiniProyecto3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author El Rey
+ */
+public class CuentasUsuarios {
+    
+    public static void main(String[] args) {
+        DefaultUser cl1 = new DefaultUser("Daniel", "001", 2000);
+        DefaultUser cl2 = new DefaultUser("Jose", "002", 3000);
+        DefaultUser cl3 = new DefaultUser("Enrique", "003", 4000);
+        DefaultUser cl4 = new DefaultUser("Range", "004", 5000);
+        DefaultUser cl5= new DefaultUser("Daniel", "001", 2000);
+
+        
+        Set <DefaultUser> clientesBanco = new HashSet<>();
+        
+        //Agregando los objetos de tipo cliente
+        clientesBanco.add(cl1);
+        clientesBanco.add(cl2);
+        clientesBanco.add(cl3);
+        clientesBanco.add(cl4);
+        clientesBanco.add(cl5);
+
+        //Recorrer collection
+        for (DefaultUser cliente : clientesBanco) {
+            
+            System.out.println(cliente.getNombre() + " " + cliente.getNumeroCuenta() + " " + cliente.getSaldo());
+            
+        }
+    }
+    
+}
---
+++ src/main/java/MiniProyecto3/DefaultUser.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MiniProyecto3;
+
+/**
+ *
+ * @author El Rey
+ */
+public class DefaultUser {
+
+    public DefaultUser(String nombre, String numeroCuenta, double saldo) {
+        this.nombre = nombre;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    private String nombre;
+    private String numeroCuenta;
+    private double saldo;
+
+    
+
+    
+    
+}
